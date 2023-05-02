ec2d5958fa3cc300f5aa0cf80bb35d8c846efda8
Harshad Hegde <hegdehb@gmail.com>
2023-05-02 18:07:21 -0500

    cleanup

--- src/openai_examples/main.py
+++ src/openai_examples/main.py
@@ -46,34 +46,6 @@ def demo(input: str):
     client = OpenAIClient()
     client.update_context(role="user", content=input)
     return client.get_completion_from_messages(client.context)
-    # text = """
-    # You should express what you want a model to do by \
-    # providing instructions that are as clear and \
-    # specific as you can possibly make them. \
-    # This will guide the model towards the desired output, \
-    # and reduce the chances of receiving irrelevant \
-    # or incorrect responses. Don't confuse writing a \
-    # clear prompt with writing a short prompt. \
-    # In many cases, longer prompts provide more clarity \
-    # and context for the model, which can lead to \
-    # more detailed and relevant outputs.
-    # """
-    # prompt = f"""
-    # Summarize the text delimited by triple backticks \
-    # into a single sentence.
-    # ```{text}```
-    # """
-    # response = client.get_completion(prompt)
-    # print(response)
-
-    # messages = [
-    #     {"role": "system", "content": "You are an assistant that speaks like Shakespeare."},
-    #     {"role": "user", "content": "tell me a joke"},
-    #     {"role": "assistant", "content": "Why did the chicken cross the road"},
-    #     {"role": "user", "content": "I don't know"},
-    # ]
-    # response = client.get_completion_from_messages(messages, temperature=1)
-    # print(response)
 
 
 if __name__ == "__main__":
