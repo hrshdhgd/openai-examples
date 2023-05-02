59cf4ae9584c4c5835faebebf11a8e94252d4125
Harshad Hegde <hegdehb@gmail.com>
2023-05-02 18:00:37 -0500

    Working chatbot

--- src/openai_examples/cli.py
+++ src/openai_examples/cli.py
@@ -1,7 +1,8 @@
 """Command line interface for openai-examples."""
-import click
 import logging
 
+import click
+from click_shell import shell
 from openai_examples import __version__
 from openai_examples.main import demo
 
@@ -10,8 +11,10 @@ __all__ = [
 ]
 
 logger = logging.getLogger(__name__)
+input_argument = click.argument("input", required=True, nargs=-1)
 
-@click.group()
+# @click.group()
+@shell(prompt='ontobot > ', intro='Starting my app...')
 @click.option("-v", "--verbose", count=True)
 @click.option("-q", "--quiet")
 @click.version_option(__version__)
@@ -30,11 +33,25 @@ def main(verbose: int, quiet: bool):
     if quiet:
         logger.setLevel(level=logging.ERROR)
 
+
+
 @main.command()
-def run():
+@click.argument("subcommand")
+@click.pass_context
+def help(ctx, subcommand):
+    """Echoes help for subcommands."""
+    subcommand_obj = main.get_command(ctx, subcommand)
+    if subcommand_obj is None:
+        click.echo("The command you seek help with does not exist.")
+    else:
+        click.echo(subcommand_obj.get_help(ctx))
+
+
+@main.command()
+@input_argument
+def run(input: str):
     """Run the openai-examples's demo command."""
-    demo()
-     
+    click.echo(demo(" ".join(input)))
 
 
 if __name__ == "__main__":
--- src/openai_examples/main.py
+++ src/openai_examples/main.py
@@ -1,9 +1,80 @@
 """Main python file."""
+import os
 
-def demo():
+import openai
+from dotenv import find_dotenv, load_dotenv
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+
+class OpenAIClient:
+    model: str = "gpt-3.5-turbo"
+    context = [
+        {
+            "role": "system",
+            "content": """You are ChatBot whose duty is to answer questions from users.""",
+        }
+    ]
+
+    def __post_init__(self):
+        if not openai.api_key:
+            openai.api_key = os.getenv("OPENAI_API_KEY_2")
+
+    def get_completion(self, prompt):
+        self.context.append({"role": "user", "content": prompt})
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=self.context,
+            temperature=0,  # this is the degree of randomness of the model's output
+        )
+        return response.choices[0].message["content"]
+
+    def get_completion_from_messages(self, messages, temperature=0):
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=messages,
+            temperature=temperature,  # this is the degree of randomness of the model's output
+        )
+        return response.choices[0].message["content"]
+
+    def update_context(self, role, content):
+        self.context.append({"role": role, "content": content})
+
+
+def demo(input: str):
     """Define API."""
-    print("Hello, World!")
+    client = OpenAIClient()
+    client.update_context(role="user", content=input)
+    return client.get_completion_from_messages(client.context)
+    # text = """
+    # You should express what you want a model to do by \
+    # providing instructions that are as clear and \
+    # specific as you can possibly make them. \
+    # This will guide the model towards the desired output, \
+    # and reduce the chances of receiving irrelevant \
+    # or incorrect responses. Don't confuse writing a \
+    # clear prompt with writing a short prompt. \
+    # In many cases, longer prompts provide more clarity \
+    # and context for the model, which can lead to \
+    # more detailed and relevant outputs.
+    # """
+    # prompt = f"""
+    # Summarize the text delimited by triple backticks \
+    # into a single sentence.
+    # ```{text}```
+    # """
+    # response = client.get_completion(prompt)
+    # print(response)
+
+    # messages = [
+    #     {"role": "system", "content": "You are an assistant that speaks like Shakespeare."},
+    #     {"role": "user", "content": "tell me a joke"},
+    #     {"role": "assistant", "content": "Why did the chicken cross the road"},
+    #     {"role": "user", "content": "I don't know"},
+    # ]
+    # response = client.get_completion_from_messages(messages, temperature=1)
+    # print(response)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     demo()
